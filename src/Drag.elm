f20fe6111ff95ca859862e22461abadf7aa35865
Connor Osborn <cdosborn@uw.edu>
2014-07-11 15:33:09 -0700

    drag signal includes better info

--- src/Drag.elm
+++ src/Drag.elm
@@ -4,16 +4,15 @@ import Mouse
 main : Signal Element
 main = asText <~ state
 
---state : Signal (Bool,(Int,Int))
---state = merge ((\point -> (False,point)) <~ mouseDragStop) 
---              ((\point -> (True,point)) <~ mouseDragStart) 
---
-mouseDragStart = keepWhen Mouse.isDown (0,0) posOnDown
-mouseDragStop = dropWhen Mouse.isDown (0,0) posOnDown
+-- state (isDown, pos of first click, pos of current relative to start)
+state : Signal (Bool,(Int,Int),(Int,Int))
+state = (,,) <~ Mouse.isDown ~ mouseFirstDown ~ relativeToStart
 
-posOnDown : Signal (Int, Int)
-posOnDown = (sampleOn Mouse.isDown Mouse.position)
+-- Signal of the position vector relative to beginning of drag
+relativeToStart = subt <~ mouseFirstDown ~ (merge posOnDown (keepWhen Mouse.isDown (0,0) Mouse.position))
 
-state = subt <~ posOnDown ~ (merge posOnDown (keepWhen Mouse.isDown (0,0) Mouse.position))
+mouseFirstDown = keepWhen Mouse.isDown (0,0) posOnDown
+posOnDown = sampleOn Mouse.isDown Mouse.position
+
 
 subt vec vec2 = (-1 * ((fst vec) - (fst vec2)), (snd vec) - (snd vec2))
